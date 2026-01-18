43e33a6e6692afa7b4c9877a014a51fdf3c3c09e
yuvaraj <yuvaraj.codes@gmail.com>
2026-01-18 13:07:01 +0530

    add links to projects and socials

--- js/config.js
+++ js/config.js
@@ -1,3 +1,17 @@
 export const PROMPT = "yuvi@dev:~$";
 export const BOOT_TITLE = "YUVI | PORTFOLIO";
 export const PAUSED_TITLE = "⏸ terminal paused";
+
+export const socials = {
+    GITHUB_URL: "https://github.com/yuvii-b",
+    LINKEDIN_URL: "https://github.com/yuvii-b",
+    INSTAGRAM_URL: "https://github.com/yuvii-b"
+};
+
+export const projects = {
+    URL_SHORTENER: "https://github.com/yuvii-b/url-shortener",
+    PASSPAL_BOT: "https://github.com/yuvii-b/passpal-bot",
+    INSTAGRAM_UNSAVER: "https://github.com/yuvii-b/instagram-unsaver",
+    TERMINAL_PORTFOLIO: "https://github.com/yuvii-b/terminal-portfolio",
+    GPA_CALCULATOR: "https://github.com/yuvii-b/gpa-calc"
+};
